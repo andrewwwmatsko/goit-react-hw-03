fe3b9114cad87bde72af08205b49a969cea69857
andrewwwmatsko <matskoandrew@gmail.com>
2024-06-25 18:50:05 +0300

    local storage

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -3,15 +3,28 @@ import SearchBox from "../SearchBox/SearchBox";
 import ContactList from "../ContactList/ContactList";
 
 import css from "./App.module.css";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 export default function App() {
-  const [contacts, setContacts] = useState([
-    { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
-    { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
-    { id: "id-3", name: "Eden Clements", number: "645-17-79" },
-    { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
-  ]);
+  const [contacts, setContacts] = useState(() => {
+    const savedContacts = window.localStorage.getItem("savedContacts");
+
+    if (savedContacts !== null) {
+      return JSON.parse(savedContacts);
+    } else {
+      return [
+        { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
+        { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
+        { id: "id-3", name: "Eden Clements", number: "645-17-79" },
+        { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
+      ];
+    }
+  });
+
+  useEffect(() => {
+    window.localStorage.setItem("savedContacts", JSON.stringify(contacts));
+  }, [contacts]);
+
   const [filter, setFilter] = useState("");
 
   const filteredContacts = contacts.filter((contact) => {
